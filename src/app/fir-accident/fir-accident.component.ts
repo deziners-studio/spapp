1c52d13bfd7fb5dcb1f3cc4f1a18b09e36f61a54
Swapnil Lale <swapnillale20@gmail.com>
2017-12-18 10:43:17 +0530

    fir accident form

---
+++ src/app/fir-accident/fir-accident.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'fir-accident',
+    templateUrl: './fir-accident.component.html',
+    styleUrls: ['./fir-accident.component.css']
+})
+export class FirAccidentComponent implements OnInit {
+
+    constructor() { }
+    
+    ngOnInit() {
+    }
+
+}
